39639a7d5b41feb0a9ed25c185d173a7d8828faf
Ankit Gupta <ankitmanojg@gmail.com>
2025-09-08 17:59:42 +0530

    Agents

--- main.py
+++ main.py
@@ -17,18 +17,20 @@ def main():
     client = genai.Client(api_key=api_key)
 
     system_prompt = """
-You are a helpful AI assistant.
+    You are a helpful AI coding agent.
 
-When a user asks a question or makes a request, make a function call plan. 
-You can perform the following operations:
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
-- get_files_info: Use this ONLY when the user asks about a directory or folder path. 
-- get_file_content: Use this ONLY when the user asks about the contents of a file path. 
-- write_file: Use this to create or update files. 
-- run_python_file: Use this to run Python files with optional arguments. 
+    - List files and directories
+    - Read file contents
+    - Execute Python files with optional arguments
+    - Write or overwrite files
 
-All paths are relative to the working directory. 
-"""
+    When the user asks about the code project - they are referring to the working directory. SO you should typically start by looking at the project's files, and figuring out how to run the project and how to run its tests, you'll always want to test the tests and the actual project to verify that behavior is working.
+
+    All paths you provide should be relative to the working directory. 
+    You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
     if len(sys.argv) < 2:
         print("I need a prompt!")
@@ -56,27 +58,46 @@ All paths are relative to the working directory.
         system_instruction=system_prompt
     )
 
-    response = client.models.generate_content(
-        model="gemini-2.5-pro",
-        contents=prompt,
-        config=config,
-    )
+    max_iters = 20
+    for i in range(0, max_iters):
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            config=config,
+        )
 
-    if response is None or response.usage_metadata is None:
-        print("response is malformed.")
-        return
-    if verbose_flag:
-        print(f"User prompt: {prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(
-            f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response is None or response.usage_metadata is None:
+            print("response is malformed.")
+            return
+        if verbose_flag:
+            print(f"User prompt: {prompt}")
+            print(
+                f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(
+                f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    if response.function_calls:
-        for function_call_part in response.function_calls:
-            result = call_function(function_call_part, verbose_flag)
-            print(result)
-    else:
-        print(response.text)
+        if response.candidates:
+            for candidate in response.candidates:
+                if candidate is None or candidate.content is None:
+                    continue
+                messages.append(candidate.content)
+
+        if response.function_calls:
+            tool_parts = []
+            for function_call_part in response.function_calls:
+                result = call_function(function_call_part, verbose_flag)
+                tool_parts.extend(result.parts)
+
+            messages.append(
+                types.Content(
+                    role="tool",
+                    parts=tool_parts,
+                )
+            )
+        else:
+            # final agent text message
+            print(response.text)
+            return
 
 
 main()
