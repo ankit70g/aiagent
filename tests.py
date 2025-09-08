8ccdeaa759cc2001730cae93ff088cbda31bc93c
Ankit Gupta <ankitmanojg@gmail.com>
2025-09-08 12:29:02 +0530

    functions

---
+++ tests.py
@@ -0,0 +1,12 @@
+# from functions.get_files_info import get_files_info
+# from functions.get_file_content import get_file_content
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file
+
+
+def main():
+    working_dir = "calculator"
+    print(run_python_file(working_dir, "main.py", ["3 + 5"]))
+
+
+main()
